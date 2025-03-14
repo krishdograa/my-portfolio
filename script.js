cb2e92fcd81ad032f5f0958f2c3ee1bc4880b599
krishdograa <krishdograa@gmail.com>
2025-03-14 18:35:36 +0530

    added media queries

--- script.js
+++ script.js
@@ -178,4 +178,24 @@ window.addEventListener("load", revealSections);
 
 
 
+function scrollToAbout(event) {
+        event.preventDefault(); // Prevent default jump
+        let aboutSection = document.getElementById("about");
+        let offset = 75; // Adjust this value between 50-100 pixels
 
+        if (aboutSection) {
+            let targetPosition = aboutSection.offsetTop - offset;
+            window.scrollTo({ top: targetPosition, behavior: "smooth" });
+        }
+    }
+
+function smoothScroll(event, sectionId) {
+        event.preventDefault(); // Prevent default anchor behavior
+        let targetSection = document.getElementById(sectionId);
+        let offset = 75; // Adjust between 50-100 pixels
+
+        if (targetSection) {
+            let targetPosition = targetSection.offsetTop - offset;
+            window.scrollTo({ top: targetPosition, behavior: "smooth" });
+        }
+    }
